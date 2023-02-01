484cab78fbe21ff72de6de09fbe05b571857dcdf
Igor Hakcolt <hakcolt@gmail.com>
2023-02-01 12:07:51 -0300

    Last optimizations to deploy

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,22 +1,7 @@
-import constants from "./Contraints"
-import { UserInput } from "./User"
-
-interface ResultData<T> {
-  message: string | undefined
-  error: string | undefined
-  statusCode: number
-  _isSuccess: boolean
-  next: string | undefined
-  data: T
-}
-
-interface Result {
-  message: string | undefined
-  error: string | undefined
-  statusCode: number
-  _isSuccess: boolean
-  next: string | undefined
-}
+import constants from "./constants"
+import { LinkInput } from "./link"
+import { Result, ResultData } from "./response"
+import { UserInput } from "./user"
 
 export { constants }
-export type { Result, ResultData, UserInput }
+export type { Result, ResultData, UserInput, LinkInput }
